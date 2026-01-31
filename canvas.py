680be861a727f991d42f404db7add6290d0f3a76
BrandomCastillo <cuetobrandom@gmail.com>
2026-01-30 18:31:57 -0600

    Avance en el paint

---
+++ canvas.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import Qt, QPoint, QRectF
+from PyQt6.QtGui import QPainter, QPen, QImage
+from PyQt6.QtGui import QColor
+class Canvas(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
+        self.image = QImage(self.size(), QImage.Format.Format_ARGB32)
+        self.image.fill(QColor("#042069"))
+        self.drawing = False
+        self.last_point = QPoint()
+        self.pen_color = QColor("#fff")
+        self.pen_width = 2
+        
---
+++ main.py
@@ -0,0 +1,14 @@
+#pip install pyqt6 pyqt6-tools
+from PyQt6 import QtWidgets, uic
+import sys
+from controler import MainController
+class Ventana(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./views/MainWindow.ui",self)
+        self.controller = MainController(self,self)
+
+app = QtWidgets.QApplication(sys.argv)
+ventana = Ventana()
+ventana.show()
+sys.exit(app.exec())
